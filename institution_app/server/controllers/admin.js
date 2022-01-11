f15cd103e4881fd994736bef6797ca788dd16801
Nikhil Reddy Vatti <35322884+sainikhil1605@users.noreply.github.com>
2022-01-11 16:07:08 +0530

    Added Auth Middleware and Admin Routes

---
+++ institution_app/server/controllers/admin.js
@@ -0,0 +1,34 @@
+const Admin = require('../models/admin');
+const { StatusCodes } = require('http-status-codes');
+
+const { CustomAPIError, UnauthorizedError } = require('../errors');
+const Login = async (req, res) => {
+  const { email, password } = req.body;
+  const admin = await Admin.findOne({ email });
+  if (!admin) {
+    throw new UnauthorizedError('User Not Found');
+  } else {
+    if (await admin.validatePassword(password)) {
+      const token = admin.generateAuthToken();
+      res.status(StatusCodes.OK).send({ access_token: token });
+    } else {
+      throw new UnauthorizedError('Invalid Credentials');
+    }
+  }
+};
+const Signup = async (req, res) => {
+  const { email } = req.body;
+  const admin = await Admin.findOne({ email });
+  if (admin) {
+    throw new UnauthorizedError('User Already Exists');
+  }
+  try {
+    const newAdmin = await Admin.create({ ...req.body });
+    const token = newAdmin.generateAuthToken();
+    res.status(StatusCodes.CREATED).send({ access_token: token });
+  } catch (err) {
+    throw new CustomAPIError(err.message, StatusCodes.INTERNAL_SERVER_ERROR);
+  }
+};
+
+module.exports = { Login, Signup };
---
+++ institution_app/server/errors/index.js
@@ -0,0 +1,10 @@
+const CustomAPIError = require('./CustomAPIError');
+const BadRequestError = require('./BadRequestError');
+const UnauthorizedError = require('./UnauthorizedError');
+const NotFoundError = require('./NotFoundError');
+module.exports = {
+  CustomAPIError,
+  BadRequestError,
+  UnauthorizedError,
+  NotFoundError,
+};
